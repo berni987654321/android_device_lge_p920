65b7e878fac0b46cb78b4b6aa0d22f966d70493b
troop <troopiii@googlemail.com>
2014-03-12 16:15:19 +0100

    cleaned up BoardConfig added stuff for twrp to init.cosmo rc changed recovery.fstab

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,6 +1,3 @@
-USE_CAMERA_STUB := false
-BOARD_USES_TI_CAMERA_HAL := true
-
 # inherit from the proprietary version
 -include vendor/lge/p920/BoardConfigVendor.mk
 
@@ -19,16 +16,20 @@ ARCH_ARM_HAVE_TLS_REGISTER := true
 NEEDS_ARM_ERRATA_754319_754320 := true
 BOARD_GLOBAL_CFLAGS += -DNEEDS_ARM_ERRATA_754319_754320
 
-#BOARD_CUSTOM_BOOTIMG_MK := device/common/uboot-bootimg.mk
+#############################BOOT##################################
 BOARD_USES_UBOOT_MULTIIMAGE := true
 BOARD_UBOOT_ENTRY := 0x80008000
 BOARD_UBOOT_LOAD := 0x80008000
+TARGET_BOOTLOADER_BOARD_NAME := p920
+BOARD_CUSTOM_BOOTIMG_MK := device/lge/p920/uboot-bootimg.mk
+###################################################################
 
+#####################Kernel##############################
 # Try to build the kernel
+BUILD_KERNEL := true
+ifdef BUILD_KERNEL
 TARGET_KERNEL_CONFIG := cyanogenmod_p920_defconfig
 TARGET_KERNEL_SOURCE := kernel/lge/omap4-common
-# Keep this as a fallback
-#TARGET_PREBUILT_KERNEL := device/lge/p920/kernel
 
 KERNEL_WL12XX_MODULES:
 	make -C hardware/ti/wlan/mac80211/compat_wl12xx/ KLIB_BUILD=$(KERNEL_OUT) ARCH="arm" $(ARM_CROSS_COMPILE) KERNEL_CROSS_COMPILE=$(ARM_CROSS_COMPILE)
@@ -47,34 +48,20 @@ KERNEL_SGX_MODULES:
 	mv $(OUT)/target/*sgx540_120.ko $(TARGET_OUT)/modules/
 
 TARGET_KERNEL_MODULES := KERNEL_SGX_MODULES KERNEL_WL12XX_MODULES
+endif
+###############################################################################
 
-## Ignore --wipe_data sent by the bootloader
-BOARD_RECOVERY_ALWAYS_WIPES := true
-BOARD_HAS_NO_SELECT_BUTTON := true
-
-# Use this flag if the board has a ext4 partition larger than 2gb
-#BOARD_HAS_LARGE_FILESYSTEM := true
-
+###########################BOARDSTUFF######################################
 BOARD_HAVE_BLUETOOTH := true
 BOARD_WPAN_DEVICE := true
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/lge/p920/bluetooth
-
-BOARD_HAS_NO_MISC_PARTITION := true
-
-#TARGET_RECOVERY_PRE_COMMAND := "/system/bin/setup-recovery"
-TARGET_RECOVERY_FSTAB = device/lge/p920/fstab.cosmo
-RECOVERY_FSTAB_VERSION = 2 
-
-TARGET_BOOTLOADER_BOARD_NAME := p920
-
+BOARD_NEEDS_CUTILS_LOG := true
 BOARD_USES_AUDIO_LEGACY := true
-
-BOARD_EGL_CFG := device/lge/p920/egl.cfg
-
-BOARD_VOLD_MAX_PARTITIONS := 16
-
+###GL
 TARGET_USES_GL_VENDOR_EXTENSIONS := false
-
+USE_OPENGL_RENDERER := true
+BOARD_EGL_CFG := device/lge/p920/egl.cfg
+###
 BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
 WPA_SUPPLICANT_VERSION           := VER_0_8_X
 BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_wl12xx
@@ -86,23 +73,18 @@ WIFI_DRIVER_MODULE_PATH          := "/system/lib/modules/wl12xx_sdio.ko"
 WIFI_DRIVER_MODULE_NAME          := "wl12xx_sdio"
 WIFI_FIRMWARE_LOADER             := ""
 USES_TI_MAC80211		 := true
-
-OMAP_ENHANCEMENT := true
-OMAP_ENHANCEMENT_CPCAM := true
-
-ifdef OMAP_ENHANCEMENT
-COMMON_GLOBAL_CFLAGS += -DOMAP_ENHANCEMENT -DTARGET_OMAP4 -DOMAP_ENHANCEMENT_CPCAM -DOMAP_ENHANCEMENT_VTC
-endif
-
-## Radio fixes
-BOARD_RIL_CLASS := ../../../device/lge/p920/ril/
-
+###FileSystem####
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 619249664
+BOARD_FLASH_BLOCK_SIZE := 131072
+BOARD_VOLD_MAX_PARTITIONS := 16
+BOARD_HAS_NO_MISC_PARTITION := true
+####Camera####
+USE_CAMERA_STUB := false
 BOARD_USES_TI_CAMERA_HAL := true
 TI_OMAP4_CAMERAHAL_VARIANT := DONOTBUILDIT
+####Omap####
 HARDWARE_OMX := true
-#FW3A := true
-#ICAP := true
-#IMAGE_PROCESSING_PIPELINE := true
 ifdef HARDWARE_OMX
 OMX_VENDOR := ti
 OMX_VENDOR_WRAPPER := TI_OMX_Wrapper
@@ -110,53 +92,36 @@ BOARD_OPENCORE_LIBRARIES := libOMX_Core
 BOARD_OPENCORE_FLAGS := -DHARDWARE_OMX=1
 endif
 
-BOARD_NEEDS_CUTILS_LOG := true
-
-TARGET_USERIMAGES_USE_EXT4 := true
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 619249664
-BOARD_FLASH_BLOCK_SIZE := 131072
-USE_OPENGL_RENDERER := true
-#BOARD_TOUCH_RECOVERY := true
-BOARD_CUSTOM_BOOTIMG_MK := device/lge/p920/uboot-bootimg.mk
+OMAP_ENHANCEMENT := true
+OMAP_ENHANCEMENT_CPCAM := true
+ifdef OMAP_ENHANCEMENT
+COMMON_GLOBAL_CFLAGS += -DOMAP_ENHANCEMENT -DTARGET_OMAP4 -DOMAP_ENHANCEMENT_CPCAM -DOMAP_ENHANCEMENT_VTC
+endif
 ENHANCED_DOMX := true
+############################################################################
 
+################RECOVERY####################################
+## Ignore --wipe_data sent by the bootloader
+BOARD_RECOVERY_ALWAYS_WIPES := true
+#TARGET_RECOVERY_PRE_COMMAND := "/system/bin/setup-recovery"
+#BOARD_TOUCH_RECOVERY := true
+#TARGET_RECOVERY_FSTAB = device/lge/p920/cosmo.fstab
+#RECOVERY_FSTAB_VERSION = 2 
+DEVICE_RESOLUTION := 480x800
+BOARD_HAS_NO_SELECT_BUTTON := true
+TARGET_RECOVERY_PIXEL_FORMAT := "BGRA_8888"
+BOARD_RECOVERY_SWIPE := true
+BOARD_UMS_LUNFILE := "/sys/devices/virtual/android_usb/android0/f_mass_storage/lun%d/file"
+#TWRP
+TW_NO_REBOOT_BOOTLOADER := true
+############################################################
 
-BOARD_HAS_VIBRATOR_IMPLEMENTATION := ../../device/lge/p920/vibrator.c
+# Radio fixes
+BOARD_RIL_CLASS := /../../device/lge/p920/ril/
+BOARD_HAS_VIBRATOR_IMPLEMENTATION := /../../device/lge/p920/vibrator.c
 
 COMMON_GLOBAL_CFLAGS += -ICS_AUDIO_BLOB -DICS_CAMERA_BLOB
 TARGET_SPECIFIC_HEADER_PATH := device/lge/p920/src-headers
 PRODUCT_VENDOR_KERNEL_HEADERS := device/lge/p920/kernel-headers
 
 COMMON_GLOBAL_CFLAGS += -DBOARD_CHARGING_CMDLINE_NAME='"chg"' -DBOARD_CHARGING_CMDLINE_VALUE='"4"'
-
-# Recovery: set depending on recovery being built for. (CWM or TWRP)
-# both init scripts can be found in the recovery folder
-TARGET_RECOVERY_INITRC := device/lge/p920/init.cosmo.rc
-
-# TWRP specific build flags
-#BOARD_USE_CUSTOM_RECOVERY_FONT:= \"roboto_15x24.h\"
-DEVICE_RESOLUTION := 480x800
-TW_INTERNAL_STORAGE_PATH := "/sdcard"
-TW_INTERNAL_STORAGE_MOUNT_POINT := "data"
-TW_EXTERNAL_STORAGE_PATH := "/sdcard/_ExternalSD"
-TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sdcard"
-#RECOVERY_SDCARD_ON_DATA := false
-#BOARD_HAS_NO_REAL_SDCARD := false
-#PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
-#TW_NO_USB_STORAGE := true
-#TWRP_EVENT_LOGGING := false
-
-#TW_INCLUDE_CRYPTO := true
-#TW_INCLUDE_CRYPTO_SAMSUNG := true
-#TW_CRYPTO_FS_TYPE := "ext4"
-#TW_CRYPTO_REAL_BLKDEV := "/dev/block/mmcblk0p16"
-#TW_CRYPTO_MNT_POINT := "/data"
-#TW_CRYPTO_FS_OPTIONS := "noatime,nosuid,nodev,discard,noauto_da_alloc,journal_async_commit,errors=panic wait,check,encryptable=footer"
-#TW_CRYPTO_FS_FLAGS := "0x00000406"
-#TW_CRYPTO_KEY_LOC := "footer"
-#TW_BRIGHTNESS_PATH := "/sys/devices/platform/s5p-dsim.0/s6evr02/backlight/panel/brightness"
-#TW_MAX_BRIGHTNESS := 255
-#TARGET_USERIMAGES_USE_EXT4 := true
-#TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/virtual/android_usb/android0/f_mass_storage/lun0/file
-#TW_INCLUDE_FB2PNG := true
-TW_FLASH_FROM_STORAGE := true
