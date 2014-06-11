7629a82c30ad51060f83aa6244f0bb8b66eaff5e
troop <troop@email.de>
2014-06-11 16:08:45 +0200

    added more wifi stuff

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -66,6 +66,9 @@ TARGET_USES_GL_VENDOR_EXTENSIONS := false
 USE_OPENGL_RENDERER := true
 BOARD_EGL_CFG := device/lge/p920/egl.cfg
 ###
+###Wifi
+USES_TI_MAC80211 := true
+COMMON_GLOBAL_CFLAGS += -DUSES_TI_MAC80211
 BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
 WPA_SUPPLICANT_VERSION           := VER_0_8_X
 BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_wl12xx
@@ -73,10 +76,12 @@ BOARD_HOSTAPD_DRIVER             := NL80211
 BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_wl12xx
 BOARD_WLAN_DEVICE                := wl12xx_mac80211
 BOARD_SOFTAP_DEVICE              := wl12xx_mac80211
+PRODUCT_WIRELESS_TOOLS           := true
 WIFI_DRIVER_MODULE_PATH          := "/system/lib/modules/wl12xx_sdio.ko"
 WIFI_DRIVER_MODULE_NAME          := "wl12xx_sdio"
-WIFI_FIRMWARE_LOADER             := ""
-USES_TI_MAC80211		 := true
+WIFI_BAND := 802_11_ABGN
+PRODUCT_PROPERTY_OVERRIDES := \
+       wifi.interface=wlan0
 ###FileSystem####
 TARGET_USERIMAGES_USE_EXT4 := true
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 619249664
